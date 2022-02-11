bc3369ed2fd188bef969323f9a9d70065f3f9bad
ferx1111 <73082358+ferx1111@users.noreply.github.com>
2022-02-11 11:11:04 +0300

    Update Twitch.cs

--- Twitch.cs
+++ Twitch.cs
@@ -16,8 +16,8 @@ namespace SteamChecker
             using (var hc = new HttpClient(hcHandle, false))
 
             {
-                hc.DefaultRequestHeaders.Add("Client-ID", "gp762nuuoqcoxypju8c569th9wz7q5");
-                hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "d12hk677h7edr9vak6oybkhk31ag38");
+                hc.DefaultRequestHeaders.Add("Client-ID", "XXXXXXXXXXXXXXXXXXXXXX"); //Set Client-ID
+                hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "XXXXXXXXXXXXXXXXXXX"); //Set Authentication Token
                 hc.Timeout = TimeSpan.FromSeconds(5);
 
                 using (var response = await hc.GetAsync($"https://api.twitch.tv/helix/streams?user_login={channel}"))
